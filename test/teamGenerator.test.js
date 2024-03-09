d4fbbd74ac29e18fcd468b38b8224baedb70f12a
Raphaël <rhauteville@medinplus.com>
2024-03-09 20:40:49 +0100

    correction  test integration

--- test/teamGenerator.test.js
+++ test/teamGenerator.test.js
@@ -17,12 +17,12 @@ describe('TeamGenerator unity', () => {
 describe('TournamentGenerator integration', () => {
     it('qualified teams from poules should move to final stages', () => {
       const teams = [
-        { name: "Team 1", players: [] },
-        { name: "Team 2", players: [] },
-        { name: "Team 3", players: [] },
-        { name: "Team 4", players: [] },
-        { name: "Team 5", players: [] },
-        { name: "Team 6", players: [] }
+        { name: "Team 1", players: ['Player1', 'Player2', 'Player3'] },
+        { name: "Team 2", players: ['Player4', 'Player5', 'Player6'] },
+        { name: "Team 3", players: ['Player7', 'Player8', 'Player9'] },
+        { name: "Team 4", players: ['Player10', 'Player11', 'Player12'] },
+        { name: "Team 5", players: ['Player13', 'Player14', 'Player15'] },
+        { name: "Team 6", players: ['Player16', 'Player17', 'Player18'] },
       ];
       const tournamentGenerator = new TournamentGenerator(teams);
       tournamentGenerator.generatePoules();
