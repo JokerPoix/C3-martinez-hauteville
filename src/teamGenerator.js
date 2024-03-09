150a3ec35cda08a3bcd7aa757dd857aa33092765
Raphaël <rhauteville@medinplus.com>
2024-03-09 22:04:34 +0100

    added balanced team to generate teamif skill in player property

--- src/teamGenerator.js
+++ src/teamGenerator.js
@@ -6,18 +6,31 @@ class TeamGenerator {
   }
 
   generateTeams() {
-    let shuffledPlayers = [...this.players].sort(() => 0.5 - Math.random()); // Mélange aléatoire des joueurs
+    const shouldBalanceTeams = this.players.every(player => player.hasOwnProperty('skill'));    
     let teamIndex = 0;
-
-    while (shuffledPlayers.length > 0) {
-      let teamPlayers = shuffledPlayers.splice(0, this.playersPerTeam);
-      let teamName = `Équipe ${teamIndex + 1}`;
-      let team = {
-        name: teamName,
-        players: teamPlayers,
-      };
-      this.teams.push(team);
-      teamIndex++;
+    if (shouldBalanceTeams) {
+      let sortedPlayers = [...this.players].sort((a, b) => b.skill - a.skill);
+      let teamSkills = new Array(Math.ceil(this.players.length / this.playersPerTeam)).fill(0);
+      sortedPlayers.forEach(player => {
+        let index = teamSkills.indexOf(Math.min(...teamSkills));
+        if (!this.teams[index]) {
+          this.teams[index] = { name: `Équipe ${index + 1}`, players: [] };
+        }
+        this.teams[index].players.push(player);
+        teamSkills[index] += player.skill;
+      });
+    } else {
+      let shuffledPlayers = [...this.players].sort(() => 0.5 - Math.random()); 
+      while (shuffledPlayers.length > 0) {
+        let teamPlayers = shuffledPlayers.splice(0, this.playersPerTeam);
+        let teamName = `Équipe ${teamIndex + 1}`;
+        let team = {
+          name: teamName,
+          players: teamPlayers,
+        };
+        this.teams.push(team);
+        teamIndex++;
+      }
     }
   }
 
